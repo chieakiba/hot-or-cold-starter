6605ac5c5dc967fe10986d27a9911cb52fa0407b
Chie <akiba.chie@gmail.com>
2016-05-10 14:05:46 -0700

    Added code to generate random numbers when user clicks 'submit' button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+
+//generate random number
+var generateRandomNumber = function() {
+	Math.floor(Math.random() *100) +1;
+};
+//show the number(s) that the user guessed so far in #guessList
+
+//get user guess
+var getUserGuess = function() {
+	'#userGuess'.val();
+};
+
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -10,13 +22,26 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-
+  	//generate a secret number between 1 and 100 that the user will have to guess after clicking on the submit button
+  	$(".button").click(function() {
+  		generateRandomNumber();
+  		//give feedback for guess in #feedback
+  			//too low/high (colder/cold), getting closer (warmer/hot), or just right
+  		if (getUserGuess >= (generateRandomNumber + 5) || (generateRandomNumber - 5)) {
+  			console.log("Hot");
+  		}
+  		else if (getUserGuess > generateRandomNumber) {
+  			console.log("Colder");
+  		}
+  		else if (getUserGuess <= generateRandomNumber) {
+  			console.log("Hot");
+  		}
+  		else if (getUserGuess < (generateRandomNumber + 5) || (generateRandomNumber - 5)) {
+  			console.log("Colder");
+  		}
+  	})
 });
 
-//generate a secret number between 1 and 100 that the user will have to guess
-
-//give feedback for guess in #feedback
-	//too low/high (colder/cold), getting closer (warmer/hot), or just right
-	//show the number(s) that the user guessed so far in #guessList
+//show the number(s) that the user guessed so far in #guessList
 
 //enable new game to start when the user clicks on .new for New Game
