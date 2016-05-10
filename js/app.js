b227f20b9c0daf1578632d3bb2aa9f5ab307eaa9
Chie <akiba.chie@gmail.com>
2016-05-10 12:38:09 -0700

    First commit. Added an outline for what to code for this app

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -14,4 +13,10 @@ $(document).ready(function(){
 
 });
 
+//generate a secret number between 1 and 100 that the user will have to guess
 
+//give feedback for guess in #feedback
+	//too low/high (colder/cold), getting closer (warmer/hot), or just right
+	//show the number(s) that the user guessed so far in #guessList
+
+//enable new game to start when the user clicks on .new for New Game
