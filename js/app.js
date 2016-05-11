6716dd4fe82e09d9728355b5e1575118f81f2e3a
Chie <akiba.chie@gmail.com>
2016-05-10 17:19:40 -0700

    Enabled 'New Game' button to refresh page so the user can play again

--- js/app.js
+++ js/app.js
@@ -1,55 +1,57 @@
 //generate random number
 var randomNumber;
 var generateRandomNumber = function() {
-	randomNumber = Math.floor(Math.random() *100) +1;
+    randomNumber = Math.floor(Math.random() * 100) + 1;
 };
 //show the number(s) that the user guessed so far in #guessList
-var counter = function () {
-        var guessCount = $('#count').text();
-        var newValue = +guessCount+1;
-        return $('#count').text(newValue);
-      };
+var counter = function() {
+    var guessCount = $('#count').text();
+    var newValue = +guessCount + 1;
+    return $('#count').text(newValue);
+};
 
-$(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-  	});
+$(document).ready(function() {
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
-  	//generate a secret number between 1 and 100 that the user will have to guess after clicking on the submit button
-  	generateRandomNumber();
-  	$(".button").click(function(event) {
-  		event.preventDefault();
-		  var getUserGuess = $('#userGuess').val();
-      var distancefromCorrectAnswer = Math.abs(randomNumber - getUserGuess);
-      counter();
-      //give feedback if userGuess is correct
-  		if (getUserGuess == randomNumber) {
-        $('#feedback').text("Correct!");
-      }
-      //give feedback if userGuess is close to the correct number
-      else if (distancefromCorrectAnswer <= 10 && distancefromCorrectAnswer >= 1) {
-        $('#feedback').text("Hot!");
-      }
-      //give feedback if userGuess is getting closer to the correct number
-      else if (distancefromCorrectAnswer <= 20 && distancefromCorrectAnswer >= 11) {
-        $('#feedback').text("Warmer");
-      }
-      //give feedback if userGuess is farther from the correct number
-      else if (distancefromCorrectAnswer <= 30 && distancefromCorrectAnswer >= 21) {
-        $('#feedback').text("Cold");
-      }
-      //give feedback if userGuess is far from the correct number
-      else {
-        $('#feedback').text("Very Cold");
-      }
-  	})
+    /*--- Display information modal box ---*/
+    $(".what").click(function() {
+        $(".overlay").fadeIn(1000);
+    });
+
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function() {
+        $(".overlay").fadeOut(1000);
+    });
+    //generate a secret number between 1 and 100 that the user will have to guess after clicking on the submit button
+    generateRandomNumber();
+    $(".button").click(function(event) {
+        event.preventDefault();
+        var getUserGuess = $('#userGuess').val();
+        var distancefromCorrectAnswer = Math.abs(randomNumber - getUserGuess);
+        counter();
+        //give feedback if userGuess is correct
+        if (getUserGuess == randomNumber) {
+            $('#feedback').text('Correct! Click "New Game" to play again.');
+        }
+        //give feedback if userGuess is close to the correct number
+        else if (distancefromCorrectAnswer <= 10 && distancefromCorrectAnswer >= 1) {
+            $('#feedback').text("Hot!");
+        }
+        //give feedback if userGuess is getting closer to the correct number
+        else if (distancefromCorrectAnswer <= 20 && distancefromCorrectAnswer >= 11) {
+            $('#feedback').text("Warmer");
+        }
+        //give feedback if userGuess is farther from the correct number
+        else if (distancefromCorrectAnswer <= 30 && distancefromCorrectAnswer >= 21) {
+            $('#feedback').text("Cold");
+        }
+        //give feedback if userGuess is far from the correct number
+        else {
+            $('#feedback').text("Very Cold");
+        }
+    })
+    //enable new game to start when the user clicks on .new for New Game
+    $('.new').click(function() {
+      window.location.reload();
+    })
 });
 
-
-//enable new game to start when the user clicks on .new for New Game
